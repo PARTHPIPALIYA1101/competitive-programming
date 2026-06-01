a356f8457bbf178a390669eb7e4ca8ce3c002d9e
PARTHPIPALIYA1101 <parthpipaliya0@gmail.com>
2026-06-01 16:33:01 +0530

    432A

---
+++ AChoosingTeams.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class AChoosingTeams {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            int y = sc.nextInt();
+            if (y + k <= 5)
+                count++;
+        }
+        System.out.println(count / 3);
+    }
+}
