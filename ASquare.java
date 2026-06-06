c5e809df1732c2a874f0766b55039ccedf7d2035
PARTHPIPALIYA1101 <parthpipaliya0@gmail.com>
2026-06-06 16:09:13 +0530

    1921A

---
+++ ASquare.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class ASquare {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-- >0){
+            int minX = Integer.MAX_VALUE, maxX = Integer.MIN_VALUE;
+            for (int i = 0; i < 4; i++) {
+                int x = sc.nextInt();
+                int y = sc.nextInt();
+                minX = Math.min(minX, x);
+                maxX = Math.max(maxX, x);
+            }
+            int side = maxX - minX;
+            System.out.println(side * side);
+        }
+    }
+}
