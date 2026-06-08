59bb9b8a5c8d5f6c6f898ccc724b7be5b5ef95b0
PARTHPIPALIYA1101 <parthpipaliya0@gmail.com>
2026-06-08 16:35:25 +0530

    1472A

---
+++ ACardsForFriends.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class ACardsForFriends {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-- >0){
+            long w = sc.nextLong();
+            long h = sc.nextLong();
+            long n = sc.nextLong();
+            long pieces = 1;
+            while (w % 2 == 0) {
+                pieces *= 2;
+                w /= 2;
+            }
+            while (h % 2 == 0) {
+                pieces *= 2;
+                h /= 2;
+            }
+            System.out.println(pieces >= n ? "YES" : "NO");
+        }
+    }
+}
