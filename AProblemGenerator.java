1ded3c0613fc2f81334225cd15046ac1709962cf
PARTHPIPALIYA1101 <parthpipaliya0@gmail.com>
2026-06-08 16:33:56 +0530

    1980A

---
+++ AProblemGenerator.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class AProblemGenerator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-- >0){
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            String s = sc.next();
+            int[] freq = new int[7];
+            for (char c : s.toCharArray()) {
+                freq[c - 'A']++;
+            }
+            int ans = 0;
+            for (int i = 0; i < 7; i++) {
+                if (freq[i] < m) ans += m - freq[i];
+            }
+            System.out.println(ans);
+        }
+    }
+}
