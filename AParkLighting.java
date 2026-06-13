6ebc822f6fff7f4751b0dff40e48be0635c0b55f
PARTHPIPALIYA1101 <parthpipaliya0@gmail.com>
2026-06-13 17:46:42 +0530

    1358A

---
+++ AParkLighting.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class AParkLighting {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-- >0){
+            long n = sc.nextLong();
+            long m = sc.nextLong();
+            System.out.println((n * m + 1) / 2);
+        }
+    }
+}
