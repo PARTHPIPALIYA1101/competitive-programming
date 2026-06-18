ca04a2c4ec9225adccdd4dc028de75e79739ace9
PARTHPIPALIYA1101 <parthpipaliya0@gmail.com>
2026-06-18 16:09:57 +0530

    2074B

--- BFileName.java
+++ BFileName.java
@@ -4,7 +4,7 @@ public class BFileName {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        String s = sc.next();
+        String s = sc.next();   
         int ans = 0, cnt = 0;
         for (int i = 0; i < n; i++) {
             if (s.charAt(i) == 'x') {
