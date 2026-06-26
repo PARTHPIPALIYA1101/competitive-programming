d3ab94323a7c8500022da0f24300331f66b4ab3b
PARTHPIPALIYA1101 <parthpipaliya0@gmail.com>
2026-06-26 19:13:40 +0530

    1660A

---
+++ AVasyaAndCoins.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class AVasyaAndCoins {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-- >0){
+            long a = sc.nextLong();
+            long b = sc.nextLong();
+            if (a == 0) System.out.println(1);
+            else System.out.println(a + 2 * b + 1);
+        }
+    }
+}
