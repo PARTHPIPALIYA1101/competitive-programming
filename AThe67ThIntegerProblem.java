61a1b23e3a85573fdc80f1c20e257ada8307b278
PARTHPIPALIYA1101 <parthpipaliya0@gmail.com>
2026-06-27 21:08:37 +0530

    2218A

---
+++ AThe67ThIntegerProblem.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class AThe67ThIntegerProblem {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-- >0){
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while (t-- > 0) {
+                sc.nextInt();
+                System.out.println(67);
+            }
+        }
+    }
+}
