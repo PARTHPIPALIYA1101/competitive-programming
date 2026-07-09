3bd98f8873a44cdcc4a0b413da9d8fb9e3d361ab
PARTHPIPALIYA1101 <parthpipaliya0@gmail.com>
2026-07-09 18:35:51 +0530

    1873D

---
+++ D1DEraser.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class D1DEraser {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-- >0){
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+            String s = sc.next();
+            int ans = 0;
+            int i = 0;
+            while (i < n) {
+                if (s.charAt(i) == 'B') {
+                    ans++;
+                    i += k;
+                } else {
+                    i++;
+                }
+            }
+            System.out.println(ans); 
+        }
+    }
+}
