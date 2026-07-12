1f4e9cde6d53eaeddcfbfcd6ab4803440a242e80
PARTHPIPALIYA1101 <parthpipaliya0@gmail.com>
2026-07-12 20:54:18 +0530

    2246A

---
+++ AFarmpiggieAndSubsetSum.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class AFarmpiggieAndSubsetSum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-- >0){
+            int n=sc.nextInt();
+            for(int i=1;i<=n;i+=2){
+                System.out.print((i+1) + " " + (i));
+                if(i + 2 <= n) System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
