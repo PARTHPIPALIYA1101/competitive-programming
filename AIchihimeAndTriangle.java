76ef6003332811827edf394528ddc67d0967fbfa
PARTHPIPALIYA1101 <parthpipaliya0@gmail.com>
2026-07-18 19:56:08 +0530

    1337A

---
+++ AIchihimeAndTriangle.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class AIchihimeAndTriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-- >0){
+            sc.nextLong();
+            long b = sc.nextLong();
+            long c = sc.nextLong();
+            sc.nextLong();
+            System.out.println(b + " " + c + " " + c);
+        }
+    }
+}
