0c1002babf3fb7859e98cb76d827b2de8005b0ba
PARTHPIPALIYA1101 <parthpipaliya0@gmail.com>
2026-07-28 12:57:14 +0530

    Contest Class 2

--- DNumberOfPairs.java
+++ DNumberOfPairs.java
@@ -3,33 +3,35 @@ import java.util.*;
 public class DNumberOfPairs {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int t=sc.nextInt();
-        while(t-- >0){
-            int n=sc.nextInt();
-            int l=sc.nextInt();
-            int r=sc.nextInt();
-            int arr[]=new int[n];
-            for(int i=0;i<n;i++){
-                arr[i]=sc.nextInt();
-            }
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int l = sc.nextInt();
+            int r = sc.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
             Arrays.sort(arr);
-            int lb=0;
-            for(int i=0;i<n;i++){
-                if(chk(arr[i],l,r)){
-                    lb=i+1;
-                }
+            long ans = 0;
+            for (int i = 0; i < n; i++) {
+                int left = Math.max(i + 1, LB(arr, l - arr[i]));
+                int right = Math.max(i + 1, LB(arr, r - arr[i] + 1));
+                ans += right - left;
             }
-            System.out.println(lb);
+            System.out.println(ans);
         }
     }
-    public static boolean chk(int ed,int l,int r){
-        int st=1;
-        int sum=0;
-        while(st < ed){
-            sum= ed-st;
-            st++;
-            if(sum >= l && sum <= r) return true;
+    public static int LB(int[] arr, int target) {
+        int low = 0;
+        int high = arr.length;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] >= target) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
         }
-        return false;
+        return low;
     }
 }
