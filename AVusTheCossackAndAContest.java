2c2577ff19d98879571e61fd060d5900f6c81b64
PARTHPIPALIYA1101 <parthpipaliya0@gmail.com>
2026-07-30 10:24:03 +0530

    1186A

---
+++ AVusTheCossackAndAContest.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class AVusTheCossackAndAContest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int k = sc.nextInt();
+        System.out.println(m >= n && k >= n ? "Yes" : "No");
+
+    }
+}
