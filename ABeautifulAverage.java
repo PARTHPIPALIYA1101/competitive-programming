44c35b01631a9394a931874c8feff8e04315dcb0
PARTHPIPALIYA1101 <parthpipaliya0@gmail.com>
2026-08-02 19:07:43 +0530

    2162A

---
+++ ABeautifulAverage.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class ABeautifulAverage {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-- >0){
+            int n = sc.nextInt();
+            int max = 0;
+            for (int i = 0; i < n; i++) {
+                max = Math.max(max, sc.nextInt());
+            }
+            System.out.println(max);
+        }
+    }
+}
