7e5b6caed63aeb90052104d40f974790385a4e45
PARTHPIPALIYA1101 <parthpipaliya0@gmail.com>
2026-08-23 22:09:23 +0530

    1843C

---
+++ Math/CSumInBinaryTree.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class CSumInBinaryTree {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-- >0){
+            long n = sc.nextLong();
+            long sum = 0;
+            while (n > 0) {
+                sum += n;
+                n /= 2;
+            }
+            System.out.println(sum);
+        }
+    }
+}
